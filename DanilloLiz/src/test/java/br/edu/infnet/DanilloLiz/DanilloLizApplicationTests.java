3eb8eb994f935357391659c06791c364e78136e5
Danillo Liz Mendes Mailan Silva <danillo.silva@al.infnet.edu.br>
2024-03-27 20:56:17 -0300

    Primeiro commit

---
+++ DanilloLiz/src/test/java/br/edu/infnet/DanilloLiz/DanilloLizApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.DanilloLiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanilloLizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
